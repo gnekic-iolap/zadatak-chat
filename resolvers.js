c39eb04138de140d6c4f119f844e2417679876fe
bush-man92 <42248918+bush-man92@users.noreply.github.com>
2019-02-09 14:50:05 +0100

    dsd

--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,6 @@
 export default{
 	Query: {
-		allUsers: (parent, args, { models }) => models.User.findALL(),
+		allUsers: (parent, args, { models }) => models.User.findAll(),
 		getUser: (parent , { username }, { models }) =>
 		models.User.findOne({
 			where:{
@@ -11,9 +11,17 @@ export default{
 
 	Mutation: {
 		createUser:(parent, args ,{ models }) => models.User.create(args),
-		updateUser:(parent, {username, newUsername}, { models }) =>
-			models.User.update({ username: newUsername }, { where : {username}}),
-		deleteUser: (parent , args , { models }) =>
-			models.User.destroy({ where:args }),
-	}
+		updateUser:(parent, { username, newUsername }, { models }) =>
+			models.User.update({ username: newUsername },
+				{ where : {
+					username,
+				},
+			}),
+		deleteUser: (parent , { id } , { models }) =>
+			models.User.destroy({
+				where: { 
+					id,
+				},
+			}),
+	},
 };
--- schema.js
+++ schema.js
@@ -7,11 +7,11 @@ export default `
 	}
 	type Query{
 		allUsers: [User!]!
-		getUser(username:String!): User!
+		getUser(username: String!): User
 	}
 	type Mutation {
-    	createUser(username: String!, password: String!, email: String!): User
+    	createUser(username: String!, password: String!, email: String): User
     	updateUser(username: String!, newUsername: String!): [Int!]!
-    	deleteUser(username: String!): Int!
+    	deleteUser(id: Int!): Int!
 	}
 `;
