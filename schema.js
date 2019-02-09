16b91ed296a2c9bc438d63997e3f48bb9201612b
bush-man92 <42248918+bush-man92@users.noreply.github.com>
2019-02-09 13:11:06 +0100

    Update schema.js

--- schema.js
+++ schema.js
@@ -4,14 +4,13 @@ export default `
 		username: String!
 		email: String!
 		password: String!
-		
 	}
 	type Query{
 		allUsers: [User!]!
 		getUser(username:String!): User!
 	}
 	type Mutation {
-    	createUser(username: String!): User
+    	createUser(username: String!, password: String!, email: String!): User
     	updateUser(username: String!, newUsername: String!): [Int!]!
     	deleteUser(username: String!): Int!
 	}
